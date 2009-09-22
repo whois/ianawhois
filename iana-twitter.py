14e00bfa52ebc1f8582437525d2ca44e68abfdea
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2009-09-22 07:19:19 +0000

    Indication of the IANA URL
    git-svn-id: https://svn.generic-nic.net/NIC-generique/iana/whois@8963 e62f009e-d044-43ae-9900-873abb462db7

--- iana-twitter.py
+++ iana-twitter.py
@@ -5,18 +5,22 @@ import sys
 import twitter
 
 authfile = "/home/bortzmeyer/.twitter/auth"
+base_url = "http://www.iana.org/domains/root/db"
 
-if len(sys.argv) != 2:
-    print >>sys.stderr, ("Usage: %s message" % sys.argv[0])
+if len(sys.argv) != 3:
+    print >>sys.stderr, ("Usage: %s TLD message" % sys.argv[0])
     sys.exit(1)
 
 if not os.path.exists(authfile):
     print >>sys.stderr, ("Cannot find %s" % authfile)
     sys.exit(1)
 
+tld = sys.argv[1]
 msg = sys.argv[1]
 auth = open(authfile)
 login = auth.readline()[:-1]
 passwd = auth.readline()[:-1]
 api = twitter.Api(username=login, password=passwd) 
-status = api.PostUpdate("IANA whois: %s" % msg)
+url = "%s/%s.html" % (base_url, tld.lower())
+status = api.PostUpdate("IANA whois: %s %s" % (msg, url))
+
