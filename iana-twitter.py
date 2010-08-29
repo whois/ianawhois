919e0585d89cb3b4507834d24f276a8839f121af
Stephane Bortzmeyer <stephane+github@bortzmeyer.org>
2010-08-29 19:11:05 +0000

    Utilisation de bit.ly pour raccourcir les URL avant envoi
    git-svn-id: https://svn.generic-nic.net/NIC-generique/iana/whois@10118 e62f009e-d044-43ae-9900-873abb462db7

--- iana-twitter.py
+++ iana-twitter.py
@@ -7,10 +7,12 @@ import encodings.idna
 
 # http://github.com/joshthecoder/tweepy
 import tweepy
+# http://code.google.com/p/python-bitly/
+import bitly
 
 authfile = "/home/bortzmeyer/.twitter/auth-ianawhois"
-base_url = "http://www.iana.org/domains/root/db"
-vcs_url = "Change: https://viewvc.generic-nic.net/viewvc.cgi/NIC-generique/iana/whois/%s?root=R%%26D&r1=%i&r2=%i"
+iana_base_url = "http://www.iana.org/domains/root/db"
+vcs_url = "https://viewvc.generic-nic.net/viewvc.cgi/NIC-generique/iana/whois/%s?root=R%%26D&r1=%i&r2=%i"
 debug = False
 
 if len(sys.argv) != 3:
@@ -29,6 +31,10 @@ consumer_secret = authdata.readline()[:-1]
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 access_key = authdata.readline()[:-1]
 access_secret = authdata.readline()[:-1]
+bitly_login = authdata.readline()[:-1]
+bitly_key = authdata.readline()[:-1]
+if not bitly_key: 
+    raise Exception("Cannot read all the auth. material in %s: not enough lines" % authfile)
 authdata.close()
 auth.set_access_token(access_key, access_secret)
 if not debug:
@@ -39,18 +45,24 @@ if string.split(msg)[0] == "update":
     import pysvn
     client = pysvn.Client()
     log = client.log("./%s" % btld)
-    revision1 = int(log[0]['revision'].number)
-    revision2 = int(log[1]['revision'].number)
-    url = (vcs_url % (btld, revision1, revision2)) + "\n" + \
-          ("New state: %s/%s.html" % (base_url, ltld))
+    revision2 = int(log[0]['revision'].number)
+    revision1 = int(log[1]['revision'].number)
+    actual_vcs_url = (vcs_url % (btld, revision1, revision2))
+    actual_iana_url = ("%s/%s.html" % (iana_base_url, ltld))
+    bitly_api = bitly.Api(login=bitly_login, apikey=bitly_key) 
+    actual_vcs_url_short = bitly_api.shorten(actual_vcs_url)
+    actual_iana_url_short = bitly_api.shorten(actual_iana_url)
+    urls = "Changes: %s New states: %s" % \
+        (actual_vcs_url_short, actual_iana_url_short)
 else:
-    url = "%s/%s.html" % (base_url, ltld)
+    urls = "%s/%s.html" % (base_url, ltld)
+    # No need to shorten it
 if ltld[0:4] == "xn--":
     utld = encodings.idna.ToUnicode(ltld)
     tld = "%s (%s)" % (utld, tld)
 tmpl = string.Template(msg)
 expanded_msg = tmpl.substitute(tld=tld)
 if not debug:
-    status = api.update_status("IANA whois: %s %s" % (expanded_msg, url))
+    status = api.update_status("IANA whois: %s %s" % (expanded_msg, urls))
 else:
-    print "IANA whois: %s %s" % (expanded_msg, url)
+    print "IANA whois: %s %s" % (expanded_msg, urls)
